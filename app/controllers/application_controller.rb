1d6765ab5314fadc98fc656b4a2dc2a7f90d668b
George Slater <georgewslater@gmail.com>
2016-04-11 00:47:55 -0300

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
   def is_admin
-  	@is_admin ||= current_user.uid == '10101488162662259' if session[:user_id]
+  	@is_admin ||= current_user.uid == '10101488162662259' || current_user.uid = '574875567' if session[:user_id]
   end
   helper_method :is_admin
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ActiveRecord::Base
+	def number_of_teams
+		if self.number_of_players == 16
+			return 2
+		else
+			return (self.number_of_players / 6)
+		end
+	end
 end
